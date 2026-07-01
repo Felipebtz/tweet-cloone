2da405905c09c99be13f8d883e7c42a4f7dc19f2
FelipeBtz <felipebtz@gmail.com>
2026-07-01 19:54:56 -0300

    implmentacao backend laravel no projeto

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Text, View, StyleSheet, FlatList,Image,  TouchableOpacity } from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { Platform } from 'react-native';
 
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-
+import axios from 'axios';
+import { formatDistanceToNowStrict } from 'date-fns';
 export default function HomeScreen({ navigation }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getAllTweets();
+  }, []);
   
 
 
-  const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
+    function getAllTweets(){
+        axios
+        .get('http://192.168.0.102:8000/api/tweets')
+        .then(response =>{
+          setData(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }
+
+//   {
+//     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//     title: 'First Item',
+//   },
+//   {
+//     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//     title: 'Second Item',
+//   },
+//   {
+//     id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//     title: 'Third Item',
+//   },
+ 
 
  
 function gotoProfile() {
@@ -40,31 +56,31 @@ function gotoNewTweet() {
 
 }
 // Molde do renderItem “FlatList, toda vez que você pegar um item da lista, desenhe ele desse jeito aqui.”
-  const renderItem = ({ item }) => (
+  const renderItem = ({ item: tweet }) => (
     <View style={styles.tweetContainer}> 
-    <TouchableOpacity onPress={() => gotoNewTweet()}>
+    <TouchableOpacity onPress={() => gotoProfile()}>
         <Image style= {styles.avatar}
          source={{
-                uri: 'https://reactnative.dev/img/tiny_logo.png',
+                uri: tweet.user.avatar,
               }}
               />        
     </TouchableOpacity>
      <View style={{ flex: 1}}>
       <TouchableOpacity style={styles.flexRow}>
         <Text numberOfLines={1} style={styles.tweetName}>
-          {item.title}
+          {tweet.user.name}
         </Text>
          <Text numberOfLines={1} style={styles.tweetHandle}>
-          @drehimself
+          @{tweet.user.username}
           </Text>
           <Text>@middot;</Text>
           <Text numberOfLines={1} style={styles.tweetHandle}>
-            9m
+            {formatDistanceToNowStrict(tweet.created_at)}
           </Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.tweetContentContainer} onPress={() => gotoSingleTweet}>
+      <TouchableOpacity style={styles.tweetContentContainer} onPress={gotoSingleTweet}>
            <Text style={styles.tweetContent}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, sit. Consequatur odit dicta ipsa consectetur ipsam quos quia minima mollitia ad pariatur eligendi, aspernatur saepe perspiciatis 
+            {tweet.body}
             </Text>
       </TouchableOpacity>
       <View style={styles.tweetEngagement}>
@@ -93,15 +109,16 @@ function gotoNewTweet() {
     </View>
   )
   return (
-// Quando fazemos um flatlist o renderitem é uma funçao que é chamada. Nesse caso ela é chama na constante e está construido o item na tela com o DATA
+// Quando fazemos um flatlist o renderitem é uma funçao que é chamada.
+//  Nesse caso ela é chama na constante e está construido o item na tela com o DATA (agora é a api)
 // O data é a lista de informações.
 //O renderItem é a função que transforma cada informação em interface.
     <View style={styles.container}>
      <FlatList
-        data={DATA}
+        data={data}
         renderItem={renderItem}
         keyExtractor={item => item.id}
-        ItemSeparatorComponent={() => <View style={styles.tweetSeparator}> </View>}
+        ItemSeparatorComponent={() => <View style={styles.tweetSeparator} />}
       />
       <TouchableOpacity style={styles.floatingButton} onPress={() => gotoNewTweet()}>
         <AntDesign name="plus" size={26} color="white" />

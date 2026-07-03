616afa68a7cf919967a2d1d87fa29084bd5237f7
FelipeBtz <felipebtz@gmail.com>
2026-07-03 12:04:50 -0300

    então nesse commit foi criado uma solucao de implementacao o loading para carregar novos tweets então a ideia foi o seguinte quando dá o setloading ele passa a ser true e ai entao carrega atraves do isRefreshing que chama a funcao

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,128 +1,156 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Text, View, StyleSheet, FlatList,Image,  TouchableOpacity } from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { Button, Text, View, StyleSheet, FlatList, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { Platform } from 'react-native';
-
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import axios from 'axios';
 import { formatDistanceToNowStrict } from 'date-fns';
+
 export default function HomeScreen({ navigation }) {
+
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   useEffect(() => {
     getAllTweets();
   }, []);
-  
 
-
-    function getAllTweets(){
-        axios
-        .get('http://192.168.0.102:8000/api/tweets')
-        .then(response =>{
-          setData(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+  function getAllTweets() {
+    axios
+      .get('http://192.168.0.102:8000/api/tweets')
+      .then(response => {
+        setData(response.data);
+        setIsLoading(false);
+        setIsRefreshing(false);
+      })
+      .catch(error => {
+        console.log(error);
+        setIsLoading(false);
+        setIsRefreshing(false);
+      });
   }
 
-//   {
-//     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-//     title: 'First Item',
-//   },
-//   {
-//     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-//     title: 'Second Item',
-//   },
-//   {
-//     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-//     title: 'Third Item',
-//   },
- 
+  function handleRefresh() {
+    setIsRefreshing(true);
+    getAllTweets();
+  }
 
- 
-function gotoProfile() {
+  // {
+  // id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+  // title: 'First Item',
+  // },
+  // {
+  // id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+  // title: 'Second Item',
+  // },
+  // {
+  // id: '58694a0f-3da1-471f-bd96-145571e29d72',
+  // title: 'Third Item',
+  // }
 
-  navigation.navigate('Profile Screen');
-}
+  function gotoProfile() {
+    navigation.navigate('Profile Screen');
+  }
 
-function gotoSingleTweet() {
-  navigation.navigate('Tweet Screen');
+  function gotoSingleTweet() {
+    navigation.navigate('Tweet Screen');
+  }
 
-}
-function gotoNewTweet() {
-  navigation.navigate('Tweet Screen');
+  function gotoNewTweet() {
+    navigation.navigate('Tweet Screen');
+  }
 
-}
-// Molde do renderItem “FlatList, toda vez que você pegar um item da lista, desenhe ele desse jeito aqui.”
+  // Molde do renderItem “FlatList, toda vez que você pegar um item da lista, desenhe ele desse jeito aqui.”
   const renderItem = ({ item: tweet }) => (
-    <View style={styles.tweetContainer}> 
-    <TouchableOpacity onPress={() => gotoProfile()}>
-        <Image style= {styles.avatar}
-         source={{
-                uri: tweet.user.avatar,
-              }}
-              />        
-    </TouchableOpacity>
-     <View style={{ flex: 1}}>
-      <TouchableOpacity style={styles.flexRow}>
-        <Text numberOfLines={1} style={styles.tweetName}>
-          {tweet.user.name}
-        </Text>
-         <Text numberOfLines={1} style={styles.tweetHandle}>
-          @{tweet.user.username}
+    <View style={styles.tweetContainer}>
+      <TouchableOpacity onPress={() => gotoProfile()}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: tweet.user.avatar }}
+        />
+      </TouchableOpacity>
+
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity style={styles.flexRow}>
+          <Text numberOfLines={1} style={styles.tweetName}>
+            {tweet.user.name}
+          </Text>
+          <Text numberOfLines={1} style={styles.tweetHandle}>
+            @{tweet.user.username}
           </Text>
           <Text>@middot;</Text>
           <Text numberOfLines={1} style={styles.tweetHandle}>
             {formatDistanceToNowStrict(tweet.created_at)}
           </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.tweetContentContainer} onPress={gotoSingleTweet}>
-           <Text style={styles.tweetContent}>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.tweetContentContainer} onPress={gotoSingleTweet}>
+          <Text style={styles.tweetContent}>
             {tweet.body}
-            </Text>
-      </TouchableOpacity>
-      <View style={styles.tweetEngagement}>
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.tweetEngagement}>
           {/* component de comentario */}
-        <TouchableOpacity style={styles.flexRow}>
-              <EvilIcons name="comment" size={22} color="gray" style={{ marginRight: 2}} />
-              <Text style={styles.textGray}>108</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.flexRow}>
+            <EvilIcons name="comment" size={22} color="gray" style={{ marginRight: 2 }} />
+            <Text style={styles.textGray}>108</Text>
+          </TouchableOpacity>
+
           {/* component de retuitar */}
-        <TouchableOpacity style={styles.flexRow, styles.ml4}>
-              <EvilIcons name="retweet" size={22} color="gray" style={{ marginRight: 2}} />
-              <Text style={styles.textGray}>108</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+            <EvilIcons name="retweet" size={22} color="gray" style={{ marginRight: 2 }} />
+            <Text style={styles.textGray}>108</Text>
+          </TouchableOpacity>
+
           {/* component de coração */}
-        <TouchableOpacity style={styles.flexRow, styles.ml4}>
-              <AntDesign name="heart" size={22} color="gray" />
-              <Text style={styles.textGray}>108</Text>
-        </TouchableOpacity>
-        {/* component de compartilhamento */}
-         <TouchableOpacity style={styles.flexRow, styles.ml4}>
-              <EvilIcons name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'} size={22} color="gray" style={{ marginRight: 2}} />
-              <Text style={styles.textGray}>108</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+            <AntDesign name="heart" size={22} color="gray" />
+            <Text style={styles.textGray}>108</Text>
+          </TouchableOpacity>
+
+          {/* component de compartilhamento */}
+          <TouchableOpacity style={[styles.flexRow, styles.ml4]}>
+            <EvilIcons
+              name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'}
+              size={22}
+              color="gray"
+              style={{ marginRight: 2 }}
+            />
+            <Text style={styles.textGray}>108</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-     </View>
     </View>
-  )
+  );
+
   return (
-// Quando fazemos um flatlist o renderitem é uma funçao que é chamada.
-//  Nesse caso ela é chama na constante e está construido o item na tela com o DATA (agora é a api)
-// O data é a lista de informações.
-//O renderItem é a função que transforma cada informação em interface.
     <View style={styles.container}>
-     <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={() => <View style={styles.tweetSeparator} />}
-      />
+
+      {/* Quando fazemos um flatlist o renderitem é uma funçao que é chamada. */}
+      {/* Nesse caso ela é chama na constante e está construido o item na tela com o DATA (agora é a api) */}
+      {/* O data é a lista de informações. */}
+      {/* O renderItem é a função que transforma cada informação em interface. */}
+
+      {isLoading ? (
+        <ActivityIndicator style={{ marginTop: 8 }} size="large" color="gray" />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+          ItemSeparatorComponent={() => <View style={styles.tweetSeparator} />}
+          refreshing={isRefreshing}
+          onRefresh={handleRefresh}
+        />
+      )}
+
       <TouchableOpacity style={styles.floatingButton} onPress={() => gotoNewTweet()}>
         <AntDesign name="plus" size={26} color="white" />
       </TouchableOpacity>
+
     </View>
   );
 }
@@ -131,55 +159,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-  
-
-   },
-   flexRow: {
+  },
+  flexRow: {
     flexDirection: 'row',
-   },
-
-   tweetContainer: {
-    flexDirection:'row',
+  },
+  tweetContainer: {
+    flexDirection: 'row',
     paddingHorizontal: 12,
     paddingVertical: 12,
-    
-   },
-   avatar: {
+  },
+  avatar: {
     width: 42,
     height: 42,
     marginRight: 8,
     borderRadius: 21,
-   },
-
-   tweetName: {
+  },
+  tweetName: {
     fontWeight: 'bold',
-    color:'#22222'
-   }, 
-
-   tweetHandle: {
+    color: '#22222',
+  },
+  tweetHandle: {
     marginHorizontal: 8,
     color: 'gray',
-
-   },
-
-   tweetContentContainer: {
-   marginTop: 4,
-    
-   },
-
-   tweetContent: {
+  },
+  tweetContentContainer: {
+    marginTop: 4,
+  },
+  tweetContent: {
     lineHeight: 20,
-   },
-   textGray: {
-    color: 'gray'
-   },
-   
-    tweetEngagement: {
+  },
+  textGray: {
+    color: 'gray',
+  },
+  tweetEngagement: {
     flexDirection: 'row',
-    alignItems:'center',
+    alignItems: 'center',
     marginTop: 12,
-   },
-   floatingButton: {
+  },
+  floatingButton: {
     width: 60,
     height: 60,
     borderRadius: 30,
@@ -189,17 +206,13 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 20,
     right: 12,
-   },
-
-   ml4: {
+  },
+  ml4: {
     marginLeft: 16,
-   },
-   tweetSeparator: {
+  },
+  tweetSeparator: {
     borderBottomWidth: 1,
     borderBottomColor: '#e4e7eb',
-   }
-
-
-   
-
+  },
 });
+s
